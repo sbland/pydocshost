c5dfb4ff3ab458b5971d05c10a0cc44958bb36b4
Sam Bland <sbland.co.uk@gmail.com>
2021-06-03 11:47:43 +0100

    Add notebooks setup

--- pydocshost/run_app.py
+++ pydocshost/run_app.py
@@ -3,20 +3,26 @@ import flask
 from .routes.client_routes import client_routes
 
 
-def get_app(static_folder="../static", docs_directory=None, *args, **kwargs) -> flask.Flask:
+def get_app(static_folder="../static", docs_directory=None, notebooks_directory=None, *args, **kwargs) -> flask.Flask:
     app = flask.Flask(__name__, static_folder=static_folder)
+
+    docs_directory = docs_directory or app.root_path + "/" + "../static/docs"
+    notebooks_directory = notebooks_directory or app.root_path + "/" + "../static/notebooks"
 
     # Client Static Paths
     @app.route("/app/<path:filename>")
     def static_proxy_app(filename):
         return app.send_static_file("app/" + filename)
 
-    docs_directory = docs_directory or app.root_path + "/" + "../static/docs"
-
     # Docs Static Paths
     @app.route("/docs/<path:filename>")
     def static_proxy_docs(filename):
         return flask.send_from_directory(docs_directory, filename)
+
+    # Notebooks Static Paths
+    @app.route("/notebooks/<path:filename>")
+    def static_proxy_notebooks(filename):
+        return flask.send_from_directory(notebooks_directory, filename)
 
     client_routes(app)
     return app
