c5c8b57679fef96e2252c2a64484af0f74c16e5d
Sam Bland <sbland.co.uk@gmail.com>
2021-06-03 09:51:44 +0100

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ class BuildPyCommand(build_py.build_py):
 
 setuptools.setup(
     name="pydocshost",
-    version="0.0.1",
+    version="0.1.0",
     author="Sam Bland",
     author_email="sblandcouk@gmail.com",
     description="Python docs api and frontend",
